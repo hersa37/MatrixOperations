b6c4024cb9798f4d25ce4c10cf2f7247f62cf316
echa <echa@192.168.1.14>
2022-04-08 23:44:56 +0700

    Code is redone to make it more compact and put all the important operations into one class.

--- src/main/java/echa/matrix/Matrix.java
+++ src/main/java/echa/matrix/Matrix.java
@@ -70,19 +70,42 @@ public class Matrix {
     }
     /**
      * Finds determinant of a matrix
-     * @param matrix the matrix whose determinant is to be found
+     * @param matrixBase the matrix whose determinant is to be found
      * @return the determinant of matrix. Uses row expansion on the 1st row
+     * @throws echa.matrix.UnsopportedMatrixException
      */
-    public static double determinant(double[][] matrix){
-        return rowExpansion(0, matrix);
+    public static double determinant(double[][] matrixBase) throws UnsopportedMatrixException{
+        double[][] matrix=copyArray(matrixBase);
+        if(order(matrix)<=3){
+            return rowExpansion(0, matrix);
+        }
+        for(int row=0;row<matrix.length;row++){
+            matrix=ElemRowOp.check(row, row,matrix);
+            for(int i=row+1;i<matrix.length;i++){
+                double scale2;
+                if(matrix[i][row]!=0){
+                    scale2=-(matrix[i][row]/matrix[row][row]);
+                    matrix=add(i, row, scale2,matrix);
+                }           
+            }
+        }
+        double determinant=1;
+        for(int row=0;row<matrix.length;row++){
+            determinant=determinant*matrix[row][row];
+        }
+        return determinant;
     }
     
-    private static double rowExpansion(int row, double[][] matrix){
+    private static double rowExpansion(int row, double[][] matrix) throws UnsopportedMatrixException{
         double[][] cofactor=cofactor(matrix);
         double determinant=0;
         
-        for(int i=0;i<matrix.length;i++){
-            determinant+=matrix[row][i]*cofactor[row][i];
+        try{
+            for(int i=0;i<matrix.length;i++){
+                determinant+=matrix[row][i]*cofactor[row][i];
+            }
+        } catch (Exception e){
+            throw new UnsopportedMatrixException("No inverse");
         }
         return determinant;
     }
@@ -99,30 +122,66 @@ public class Matrix {
     }
     
     /**
-     * Calculates inverse of a matrix using 
-     * @param matrixBase the matrix used to find its inverse
+     * Calculates inverse of a matrix 
+     * @param matrix the matrix used to find its inverse
      * @return the inverse
      * @throws echa.matrix.UnsopportedMatrixException
      */
-    public static double[][] inverse(double[][] matrixBase) throws UnsopportedMatrixException{
-        if(matrixBase.length!=matrixBase[0].length){
+    public static double[][] inverse(double[][] matrix) throws UnsopportedMatrixException{
+        if(!checkSquare(matrix)){
             throw new UnsopportedMatrixException("Not square matrix");
         }
-        int order=order(matrixBase);
-        
-        double[][] matrix=copyArray(matrixBase);
-        double[][] inverse=new double[order][order];
+        if(matrix.length<=3){
+            return inverseRowExp(matrix);
+        }else return inverseElemOp(matrix);
+    }
+    
+    private static double[][] inverseRowExp(double[][] matrix) throws UnsopportedMatrixException{
+        double determinant=determinant(matrix);
+        if(determinant==0){
+            return matrix;
+        }
+        double[][] inverse=new double[matrix.length][matrix.length];
         double[][] adjoint=adjoint(matrix);
-        double determinant=rowExpansion(0,matrix);
-        
-        for(int i=0;i<order;i++){
-            for(int j=0;j<order;j++){
+
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix.length;j++){
                 inverse[i][j]=(1/determinant)*adjoint[i][j];
+            }
+        }
+        return inverse;
+
+    }
+    
+    private static double[][] inverseElemOp(double[][] matrix) throws UnsopportedMatrixException{
+        if(determinant(matrix)==0){
+            return matrix;
+        }
+        double[][] matrixTemp=GaussJordan(compoundMatrix(matrix));
+        double[][] inverse=new double[matrix.length][matrix.length];
+        for(int row=0;row<inverse.length;row++){
+            for(int column=0;column<inverse.length;column++){
+                inverse[row][column]=matrixTemp[row][column+inverse.length];
             }
         }
         return inverse;
     }
     
+    private static double[][] compoundMatrix(double[][] matrix){
+        double[][] matrixTemp=new double[matrix.length][2*matrix.length];
+        for(int row=0;row<matrix.length;row++){
+            for(int column=0;column<matrix[0].length;column++){
+                matrixTemp[row][column]=matrix[row][column];
+            }
+            for(int column=matrix.length;column<matrixTemp[0].length;column++){
+                if(column==row+matrix.length){
+                    matrixTemp[row][column]=1;
+                }
+            }
+        }
+        return matrixTemp;
+    }
     /**
      * Applies Gauss operation on a matrix
      * @param matrixBase the matrix used to do Gauss operation
@@ -148,6 +207,11 @@ public class Matrix {
         return matrix;
     }
     
+    /**
+     * Applies Gauss-Jordan operation on a matrix
+     * @param matrixBase the matrix used to do Gauss-Jordan operation
+     * @return the Gauss-Jordan matrix
+     */
     public static double[][] GaussJordan(double[][] matrixBase){
         double[][] matrix=Gauss(matrixBase);
         for(int row=matrix.length-1;row>=0;row--){
@@ -159,9 +223,15 @@ public class Matrix {
         return matrix;
     }
     
+    private static boolean checkSquare(double[][] matrix){
+        return(matrix.length==matrix[0].length);
+    }
     
-    
-    
+    /**
+     * Prints matrix in grid format
+     * @param matrix the matrix to print
+     * @return string format of matrix
+     */
     public static String toString(double [][] matrix){
         String print="";
         for (double[] matrix1 : matrix) {
--- src/main/java/echa/matrix/MatrixOperations.java
+++ src/main/java/echa/matrix/MatrixOperations.java
@@ -5,6 +5,7 @@
 
 package echa.matrix;
 
+import static echa.matrix.ElemRowOp.copyArray;
 import java.util.Arrays;
 import echa.matrix.Matrix.*;
 
@@ -17,14 +18,19 @@ public class MatrixOperations {
 
     /**
      * @param args the command line arguments
+     * @throws echa.matrix.UnsopportedMatrixException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsopportedMatrixException {
         double[][] matrix = {
-            {-1,0,-1},
-            {0,2,3},
-            {1,0,-1}
+            {1,3,0,0},
+            {0,1,0,0},
+            {0,0,1,0},
+            {0,0,0,1}
             };
-        System.out.println(Matrix.toString(Matrix.GaussJordan(matrix)));
+        
+        
+        System.out.println(Matrix.toString(Matrix.inverse(matrix)));
+        System.out.println("");
         
 //        GaussJordan gj=new GaussJordan(matrix);
 //        System.out.println(gj.toString());
